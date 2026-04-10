e698866b13a2da21d704f1ce00ab7bae2cefb990
Liam Palmqvist <Liam.palmqvist@icloud.com>
2026-04-10 13:39:33 +0100

    Add custom Ring Buffer, begin file comparer

---
+++ source/custom/CustomClass.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Liam on 10/02/2026.
+//
+
+#include "../core/CustomClass.hpp"
+
+namespace Nes
+{
+    namespace Core
+    {
+        CustomClass::CustomClass()
+        {
+            std::cout << "Hello, World!" << std::endl;
+        }
+    }
+}
---
+++ source/custom/CustomClass.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Liam on 10/02/2026.
+//
+
+#ifndef NESTOPIA_CUSTOMCLASS_HPP
+#define NESTOPIA_CUSTOMCLASS_HPP
+#pragma once
+
+#include <iostream>
+
+
+namespace Nes
+{
+    namespace Core
+    {
+        class CustomClass
+        {
+        public:
+            CustomClass();
+        };
+    }
+}
+
+#endif //NESTOPIA_CUSTOMCLASS_HPP
